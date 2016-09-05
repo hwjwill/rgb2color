3dbbc9f75320fadd0fcd31ef8e17264c1d6f8871
Will Huang <hwjwill@berkeley.edu>
2016-09-05 00:19:54 -0700

    Added auto cropping

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rgb2color
-Convert images in R, G, B channels into a colored image
+Convert images in R, G, B channels from the Prokudin-Gorskii photo collection into a colored image. Auto aligned using NCC and customized image pyramid.
+
+# To run the code
+main(‘<name of image file>’)
--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-function [] = create(imname)
+function [] = main(imname)
 % name of the input file
 %imname = 'tobolsk.jpg';
 %imname = 'nativity.jpg';
@@ -14,13 +14,17 @@ fullim = im2double(fullim);
 % compute the height of each part (just 1/3 of total)
 height = floor(size(fullim,1)/3);
 width = size(fullim,2);
-% separate color channels
+x_min = 0.1;
+x_max = 0.95;
+y_min = 0.1;
+y_max = 0.99;
+% separate clor channels
 B = fullim(1:height,:);
-B = imcrop(B, [floor(width * 0.1), floor(height * 0.2), round(width * 0.85), round(height * 0.8) ]);
+B = imcrop(B, [floor(width * x_min), floor(height * y_min), round(width * (x_max - x_min)), round(height * (y_max - y_min)) ]);
 G = fullim(height+1:height*2,:);
-G = imcrop(G, [floor(width * 0.1), floor(height * 0.2), round(width * 0.85), round(height * 0.8) ]);
+G = imcrop(G, [floor(width * x_min), floor(height * y_min), round(width * (x_max - x_min)), round(height * (y_max - y_min)) ]);
 R = fullim(height*2+1:height*3,:);
-R = imcrop(R, [floor(width * 0.1), floor(height * 0.2), round(width * 0.85), round(height * 0.8) ]);
+R = imcrop(R, [floor(width * x_min), floor(height * y_min), round(width * (x_max - x_min)), round(height * (y_max - y_min)) ]);
 
 % Align the images
 % Functions that might be useful to you for aligning the images include: 
@@ -29,12 +33,21 @@ R = imcrop(R, [floor(width * 0.1), floor(height * 0.2), round(width * 0.85), rou
 
 G = circshift(G, [xShiftG, yShiftG]);
 R = circshift(R, [xShiftR, yShiftR]);
-%%%%%aG = align(G,B);
-%%%%%aR = align(R,B);
+
 %G = zeros(size(G));
 result = cat(3, R, G, B);
+[cropped] = autoCrop(result);
+
+fileName = imname(1:end - 4);
+fileNameOriginal = strcat(fileName, 'Colored.jpg');
+imwrite(result, fileNameOriginal);
+fileNameCropped = strcat(fileName, 'Cropped.jpg');
+imwrite(cropped, fileNameCropped);
+
 figure(1);
 imshow(result);
+figure(2);
+imshow(cropped);
 
 % save result image
 %imwrite(colorim,['result-' imname]);
@@ -77,9 +90,7 @@ if size(R, 1) * size(R, 2) < threshold
     end
 %     G = circshift(G, [xShiftG, yShiftG]);
 %     R = circshift(R, [xShiftR, yShiftR]);
-%     %%%%%aG = align(G,B);
-%     %%%%%aR = align(R,B);
-%     
+% 
 %     result = cat(3, R, G, B);
 %     figure(1);
 %     imshow(result);
@@ -126,11 +137,82 @@ else
     end
 %     G = circshift(G, [xShiftG, yShiftG]);
 %     R = circshift(R, [xShiftR, yShiftR]);
-%     %%%%%aG = align(G,B);
-%     %%%%%aR = align(R,B);
 %     
 %     result = cat(3, R, G, B);
 %     figure(1);
 %     imshow(result);
 end
+end
+
+%% Autocrop method
+function [cropped] = autoCrop(input)
+resultGray = rgb2gray(input);
+scale = 1/8;
+edgeMinPercentage = 0.5;
+if size(resultGray, 1) * size(resultGray, 2) > 950000
+    resultGray = imresize(resultGray, scale);
+end
+resultGray = edge(resultGray, 'Canny');
+% figure(2);
+% imshow(resultGray);
+% dlmwrite('edge.txt', resultGray);
+xmin = 1;
+for a = 2:floor(size(resultGray, 2) * 0.1)
+    count = 0;
+    for b = 1:size(resultGray, 1)
+        if resultGray(b, a) == 1 || resultGray(b, a - 1) == 1 || resultGray(b, a + 1) == 1
+            count = count + 1;
+        end
+    end
+    if count > size(resultGray, 1) * edgeMinPercentage
+        xmin = a;
+    end
+end
+
+xmax = size(resultGray, 2);
+for a = size(resultGray, 2) - 1:-1:floor(size(resultGray, 2) * 0.95);
+    count = 0;
+    for b = 1:size(resultGray, 1)
+        if resultGray(b, a) == 1 || resultGray(b, a - 1) == 1 || resultGray(b, a + 1) == 1
+            count = count + 1;
+        end
+    end
+    if count > size(resultGray, 1) * edgeMinPercentage
+        xmax = a;
+    end
+end
+
+ymin = 1;
+for a = 2:floor(size(resultGray, 1) * 0.05)
+    count = 0;
+    for b = 1:size(resultGray, 2)
+        if resultGray(a, b) == 1 || resultGray(a - 1, b) == 1 || resultGray(a + 1, b) == 1
+            count = count + 1;
+        end
+    end
+    if count > size(resultGray, 2) * edgeMinPercentage
+        ymin = a;
+    end
+end
+
+
+ymax = size(resultGray, 1);
+for a = size(resultGray, 1) - 1:-1:floor(size(resultGray, 1) * 0.95);
+    count = 0;
+    for b = 1:size(resultGray, 2)
+        if resultGray(a, b) == 1 || resultGray(a - 1, b) == 1 || resultGray(a + 1, b) == 1
+            count = count + 1;
+        end
+    end
+    if count > size(resultGray, 2) * edgeMinPercentage
+        ymax = a;
+    end
+end
+if size(input, 1) * size(input, 2) > 950000
+    xmin = xmin / scale;
+    xmax = xmax / scale;
+    ymin = ymin / scale;
+    ymax = ymax / scale;
+end
+cropped = imcrop(input, [xmin, ymin, xmax - xmin, ymax - ymin]);
 end
